6bb4c3343a8a6c886a828bc7608a7e767d257451
Rahmtin <rr592@cornell.edu>
2014-12-22 10:49:46 -0500

    Added one plot

--- RScripts/xp_showing_up_on_timeline.R
+++ RScripts/xp_showing_up_on_timeline.R
@@ -6,20 +6,30 @@ xp_distribution = read.table(paste(directory, dataset, "_experiences_showing_up_
 colnames(xp_distribution) = c("word", "CurrentExp", "FinishingExp", "Time")
 
 
-xp_distribution$CurrentBucket = floor(xp_distribution$CurrentExp / 10)
-xp_distribution$FinishingBucket = floor(xp_distribution$FinishingExp / 10)
+xp_distribution$CurrentBucket = floor(xp_distribution$CurrentExp / 20)
+xp_distribution$FinishingBucket = floor(xp_distribution$FinishingExp / 20)
 
 head(xp_distribution)
 my_summary  = ddply(xp_distribution, c("CurrentBucket"), summarize, N = length(Time),  mean = mean(Time), sd = sd(Time), se = sd / sqrt(N))
 my_summary[is.na(my_summary)] = 0
 my_summary 
 pd = position_dodge(.1)
-jpeg(paste(directory,"AggregationPlots/", dataset, "_mean_appereance_of_each_xp.jpg",sep=""), height=800, width=1300)
+jpeg(paste(directory,"AggregationPlots/", dataset, "_mean_appereance_of_present_xp.jpg",sep=""), height=800, width=1300)
 ggplot(data = my_summary, aes(x = CurrentBucket, y=mean, colour = "blue")) + 
     geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1, position=pd) +
-    geom_line(position=pd) +  ggtitle("Mean time vs Experience bucket of size 10") +
+    geom_line(position=pd) +  ggtitle("Mean time vs Experience bucket of size 20") +
     scale_colour_hue(name="Legend", # Legend label, use darker colors
                      labels=c("Mean of current experience"),
+                     l=40) +                  # Use darker colors, lightness=40
+    geom_point(position=pd)
+dev.off()
+
+jpeg(paste(directory,"AggregationPlots/", dataset, "_mean_appereance_of_final_xp.jpg",sep=""), height=800, width=1300)
+ggplot(data = my_summary, aes(x = FinishingBucket, y=mean, colour = "blue")) + 
+    geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1, position=pd) +
+    geom_line(position=pd) +  ggtitle("Mean time vs Experience bucket of size 20") +
+    scale_colour_hue(name="Legend", # Legend label, use darker colors
+                     labels=c("Mean of Final experience"),
                      l=40) +                  # Use darker colors, lightness=40
     geom_point(position=pd)
 
