79dba1419b80f8b57b41d4c3953f58052d4a507c
Rahmtin <rr592@cornell.edu>
2014-12-22 10:56:17 -0500

    fixed xp plot

--- RScripts/xp_showing_up_on_timeline.R
+++ RScripts/xp_showing_up_on_timeline.R
@@ -9,13 +9,11 @@ colnames(xp_distribution) = c("word", "CurrentExp", "FinishingExp", "Time")
 xp_distribution$CurrentBucket = floor(xp_distribution$CurrentExp / 20)
 xp_distribution$FinishingBucket = floor(xp_distribution$FinishingExp / 20)
 
-head(xp_distribution)
-my_summary  = ddply(xp_distribution, c("CurrentBucket"), summarize, N = length(Time),  mean = mean(Time), sd = sd(Time), se = sd / sqrt(N))
+my_summary  = ddply(xp_distribution, c("CurrentBucket"), summarize, N = length(Time),  mean_time = mean(Time), sd = sd(Time), se = sd / sqrt(N))
 my_summary[is.na(my_summary)] = 0
-my_summary 
 pd = position_dodge(.1)
 jpeg(paste(directory,"AggregationPlots/", dataset, "_mean_appereance_of_present_xp.jpg",sep=""), height=800, width=1300)
-ggplot(data = my_summary, aes(x = CurrentBucket, y=mean, colour = "blue")) + 
+ggplot(data = my_summary, aes(x = CurrentBucket, y=mean_time, colour = "blue")) + 
     geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1, position=pd) +
     geom_line(position=pd) +  ggtitle("Mean time vs Experience bucket of size 20") +
     scale_colour_hue(name="Legend", # Legend label, use darker colors
@@ -24,8 +22,10 @@ ggplot(data = my_summary, aes(x = CurrentBucket, y=mean, colour = "blue")) +
     geom_point(position=pd)
 dev.off()
 
+my_summary  = ddply(xp_distribution, c("FinishingBucket"), summarize, N = length(Time),  mean_time = mean(Time), sd = sd(Time), se = sd / sqrt(N))
+my_summary[is.na(my_summary)] = 0
 jpeg(paste(directory,"AggregationPlots/", dataset, "_mean_appereance_of_final_xp.jpg",sep=""), height=800, width=1300)
-ggplot(data = my_summary, aes(x = FinishingBucket, y=mean, colour = "blue")) + 
+ggplot(data = my_summary, aes(x = FinishingBucket, y=mean_time, colour = "blue")) + 
     geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1, position=pd) +
     geom_line(position=pd) +  ggtitle("Mean time vs Experience bucket of size 20") +
     scale_colour_hue(name="Legend", # Legend label, use darker colors
