4f497fff046820bb60cf85f399159357416ede7b
Rahmtin <rr592@cornell.edu>
2015-04-06 09:37:57 -0400

    Added code for a_b heat map on Amazon

---
+++ RScripts/Amazon_a_b_heat_map.R
@@ -0,0 +1,39 @@
+library("gplots")
+library("reshape2")
+library("ggplot2")
+library("scales")
+
+draw_heat_map <- function(dataset) {
+    a_b = read.table(paste("../Output_All/",dataset, "_Bursts/RealTime/MaxBenefit/",dataset,"a_b_top_500_innovations.txt", sep=""), header=TRUE)
+    mat = matrix(rep(c(0), 201 * 201), nrow=201, ncol=201)
+    for(i in 1:nrow(a_b)) { 
+        x = c(0, 0.25, 0.5, 0.75, 1) 
+        y = c(0, a_b[i, ]$cq1, a_b[i, ]$cq2, a_b[i, ]$cq3, a_b[i, ]$cq4)
+        K = lm(y ~ x)
+        a = a_b[i, ]$a + 100
+        b = a_b[i, ]$b + 100
+        mat[a, b] = K$coefficients[2]
+    }
+    print("a_b_final_count")
+    for(i in -100:100){
+        cat(i, " " , i+1, " ", mat[i + 100,i + 101], "\n")
+    }
+    for(i in -100:95){
+        cat(i, " " , i+5, " ", mat[i + 100,i + 105], "\n")
+    }
+
+    print("________________________________")
+    smoothed = mat 
+
+    png(file="~/Pictures/", dataset, "a_b_top_500_innovations.png", height = 1600, width = 1600, res = 150)
+    dat <- melt(smoothed)
+    colnames(dat) = c("a", "b", "value")
+    dat[dat$a >= dat$b, "value"] = -2 
+    ggplot(dat, aes(x=a,y=b)) + 
+    geom_tile(aes(fill = value),colour='black') +
+    scale_fill_gradientn(colours=c("green","yellow","red")) +  
+    coord_equal() 
+    dev.off()
+}
+
+draw_heat_map("Music")
