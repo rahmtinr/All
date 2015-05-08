ce131353d1b07c5a935aafecf15a19e67f9fc750
rahmtin <rr592@cornell.edu>
2015-05-08 11:32:23 -0400

    Rscripts

--- bash.sh
+++ bash.sh
@@ -18,7 +18,7 @@ BurstMode=("MaxBenefit")
 StateMachine=("DocRatio")
 StateMachineCoeff=("3.0" "3.1" "3.2" "3.3" "3.4" "3.5" "3.6" "3.7" "3.8" "3.9" "4.0" "4.1" "4.2" "4.3" "4.4" "4.5")
 
-ExperienceState=("final", "current")
+ExperienceState=("final")
 for x in ${InputFiles[*]}; do
     input=$input_directory$x.txt;
     if [[ $x == "reddit_funny" ]]
